898e80942152ebc906db0015ec5dc6abf12adf0e
Sriram Thiruveedhula <sriramthiruveedhula@Srirams-MacBook-Air.local>
2026-04-09 01:56:06 +0530

    Fix 401 error on all maps: replace Stadia Maps with free CARTO dark tiles everywhere

--- frontend/src/components/auth-map/AnimatedMap.jsx
+++ frontend/src/components/auth-map/AnimatedMap.jsx
@@ -14,7 +14,10 @@ export default function AnimatedMap({ children }) {
         doubleClickZoom={false}
         style={{ width: '100%', height: '100%', position: 'absolute', inset: 0, zIndex: 1 }}
       >
-        <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" />
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        />
       </MapContainer>
       
       {/* Dark gradient overlay to blend into the login UI nicely */}
